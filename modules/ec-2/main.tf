3477389578ddfdf3586e45f0142f26f4655f5ab4
rishi01-11 <136174995+rishi01-11@users.noreply.github.com>
2023-06-27 21:00:34 +0530

    Add files via upload

--- modules/ec-2/main.tf
+++ modules/ec-2/main.tf
@@ -1,1 +1,19 @@
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
+  name = "first-instance"
+
+  ami = "ami-022e1a32d3f742bd8"
+
+
+  instance_type = var.instance_type
+  #key_name               = "user1"
+  monitoring = true
+  #vpc_security_group_ids = ["sg-12345678"]
+  #subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
